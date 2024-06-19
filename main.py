98dcb1af427ea99b1ddc7bf83a342b616d2fa32a
csy10001 <fuitshine@gmail.com>
2024-06-19 14:56:46 +0900

    로그링 인증 기능 추가

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 
-from fastapi import FastAPI,UploadFile,Form
+from fastapi import FastAPI,UploadFile,Form,Response,Depends
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from fastapi.staticfiles import StaticFiles
+from fastapi_login import LoginManager
+from fastapi_login.exceptions import InvalidCredentialsException
 from typing import Annotated
 import sqlite3
 
@@ -11,22 +13,39 @@ cur = con.cursor()
 
 app = FastAPI() 
 
-@app.post('/items')
-async def create_item(image:UploadFile, 
-                title:Annotated[str,Form()], 
-                price:Annotated[int, Form()], 
-                description:Annotated[str,Form()], 
-                place:Annotated[str,Form()]
-                ):
+SECRET = "super-coding"
+manager = LoginManager(SECRET,'/login')
+
+@manager.user_loader()
+def query_user(data):
+    WHERE_STATEMENTS = f'id="{data}"'
+    if type(data) == dict:
+        WHERE_STATEMENTS = f'id="{data['id']}"'
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    user = cur.execute(f"""
+                       SELECT * from users WHERE id ='{WHERE_STATEMENTS}'
+                       """).fetchone()
+    return user
+
+@app.post('/login')
+def login(id:Annotated[str,Form()], 
+           password:Annotated[str,Form()]):
+    user = query_user(id)
+    if not user:
+        raise InvalidCredentialsException
+    elif password != user['password']:
+        raise InvalidCredentialsException
     
-    image_bytes = await image.read()
-    cur.execute(f"""
-                INSERT INTO items(title,image,price,description,place)
-                VALUES ('{title}','{image_bytes.hex()}',{price},'{description}','{place}')
-                """)
-    con.commit()
+    access_token = manager.create_access_token(data={
+        'sub': {
+            'id':user['id'],
+            'name':user['name'],
+            'email':user['email']
+        }
+    })
+    return {'access_token':access_token}
     
-    return '200'
 
 @app.post('/signup')   
 def signup(id:Annotated[str,Form()], 
@@ -41,8 +60,28 @@ def signup(id:Annotated[str,Form()],
     print(id, password)
     return '200'
 
+@app.post('/items')
+async def create_item(image:UploadFile, 
+                title:Annotated[str,Form()], 
+                price:Annotated[int, Form()], 
+                description:Annotated[str,Form()], 
+                place:Annotated[str,Form()],
+                insertAt:Annotated[int,Form()],user=Depends(manager)
+                ):
+    
+    image_bytes = await image.read()
+    cur.execute(f"""
+                INSERT INTO items(title,image,price,description,place)
+                VALUES ('{title}','{image_bytes.hex()}',{price},'{description}','{place}')
+                """)
+    con.commit()
+    
+    return '200'
+
+
+
 @app.get('/items')
-async def get_items():
+async def get_items(user=Depends(manager)):
     con.row_factory = sqlite3.Row
     cor = con.cursor()
     rows = cur.execute(f"""
